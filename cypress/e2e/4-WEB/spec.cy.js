80f3c60df4b21db2a5b8d6f610fc8b9ab63245ad
Reza <Reza@gmal.com>
2024-05-13 12:29:46 +0700

    Revert "sudah banyak main di cypress"
    This reverts commit 65fc76cd7054ef8e1e6d833f5ed63e2df16b9df1.

--- cypress/e2e/4-WEB/spec.cy.js
+++ cypress/e2e/4-WEB/spec.cy.js
@@ -1,8 +1,8 @@
 describe('Sauce Demo Test', () => {
-  it('berhasil', () => {
-    cy.visit('https://www.saucedemo.com/')
-    cy.get('#user-name').type('standard_user')
+  it('passes', () => {
+    cy.visit('https://www.saucedemo.com')
+    cy.get('#user-name').type('standard_')
     cy.get('[data-test = "password"]').type('secret_sauce')
-    cy.get('.submit-button btn_action').click()
+    cy.get('.submit-button.btn_action').click()
   })
 })
